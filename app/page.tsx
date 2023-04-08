0fc2482872f7d086d277c6154eb9fd30c02d6f64
m.shaterabadi <masoodsh77@gmail.com>
2023-04-08 16:50:56 +0330

    feat(setup):enviroments setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <div className="text-rose-300 text-2xl">Helloo</div>
+    </>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
